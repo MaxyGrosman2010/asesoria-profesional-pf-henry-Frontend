33484f9c3293addcba42f28710887d2821527f9f
FedeOrefici <federicoaorefici@gmail.com>
2023-06-16 23:10:52 -0300

    firstCommitFront

---
+++ client/asesProf/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <div className="bg-slate-300 h-[80px] flex items-center justify-center">
+      <p className="font-bold uppercase">componente app</p>
+    </div>
+  )
+}
+
+export default App
