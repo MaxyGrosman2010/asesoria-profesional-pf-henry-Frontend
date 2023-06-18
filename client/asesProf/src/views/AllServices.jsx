4d2af91303a6eeac81b0b37ea64e732ad57784f2
santi carrizo <santiagocarrizo53@gmail.com>
2023-06-18 20:06:08 -0300

    18-6

--- client/asesProf/src/views/AllServices.jsx
+++ client/asesProf/src/views/AllServices.jsx
@@ -1,12 +1,46 @@
-
+import React from "react";
+import { useEffect, useState } from "react";
+import "./allServices.css";
+import Card from "./Card";
 
 const AllServices = () => {
   return (
-    <div>
-        <p>all services</p>
-        <input type='text' />
+    <div class="flex flex-col justify-center items-center">
+      <div class="flex justify-center mt-10 w-1/2 items-center h-1/20">
+        <input type="text" placeholder="type a service..." class="p-2 rounded-10 border-none outline-none" />
+        <button
+          type="submit"
+          class="py-1.5 px-4"
+        >
+          SEARCH
+        </button>
+      </div>
+      <div className="filters">
+        <select>
+          <option>by name</option>
+          <option value="asc">A-Z</option>
+          <option value="desc">Z-A</option>
+        </select>
+        <select>
+          <option>by price</option>
+          <option value="major">Major</option>
+          <option value="minor">Minor</option>
+        </select>
+        <button>show all services</button>
+      </div>
+      <div className="card"><Card/></div>
+      <div className="paginado">
+       <ul>
+        <li>
+          <button>Prev</button>
+        </li>
+        <li>
+          <button>Next</button>
+        </li>
+       </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllServices
+export default AllServices;
