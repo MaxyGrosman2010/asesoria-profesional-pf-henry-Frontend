d6259d3c4809cf7c05e5de654cc31351bc583ea0
goku673 <zapatamorato@gmail.com>
2023-06-20 10:46:19 -0400

    completado

--- client/asesProf/src/components/Home.jsx
+++ client/asesProf/src/components/Home.jsx
@@ -1,9 +1,12 @@
-
+import bgd from '../assets/background.jpg';
+import Services from './Services';
 
 const Home = () => {
   return (
-    <div>
-      <p>HOME</p>
+    <div className='flex flex-col w-full h-screen bg-cover bg-center' style={{backgroundImage:`url(${bgd})`}}>
+      
+
+      <Services/>
     </div>
   )
 }
--- client/asesProf/src/components/Navbar.jsx
+++ client/asesProf/src/components/Navbar.jsx
@@ -1,56 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const linkStyles = {
-    base: 'text-white hover:text-blue-200 px-3 py-2 text-sm font-medium',
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <nav className="bg-blue-500">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex-shrink-0">
-            <img
-              className="h-8 w-auto"
-              
-              alt="logo"
-            />
-          </div>
-          <div className="hidden md:flex ml-10 items-center space-x-4">
-            <Link to="/home" className={linkStyles.base}>
-              Home
-            </Link>
-            <div className="relative">
-              <button className={linkStyles.base}>
-              <Link to="/services" className={linkStyles.base}>
-              Services
-            </Link>
-                <svg
-                  className="ml-1 h-4 w-4 inline-block"
-                  fill="white"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
-              </button>
+    <nav className="bg-red border-gray-900">
+      <div className="max-w-screen-xl mx-auto flex items-center justify-between p-4">
+        <div className="flex items-center">
+          <img className="h-8 w-auto" alt="logo" />
+        </div>
+
+        <div className="flex items-center justify-start">
+          <ul className="flex gap-5">
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+
+            <div className="flex">
+              <div className="flex items-center bg-white w-48 justify-around py-2">
+                <span className="material-symbols-outlined"onClick={toggleMenu}>account_circle</span>
+                <button onClick={toggleMenu} className="ml-2">User</button>
+              </div>
+             
+              {menuOpen && (
+                <div className="absolute mt-10 w-48 bg-white border border-gray-300 divide-y divide-gray-200 rounded-md shadow-lg">
+                  <ul className="py-2">
+                    <li>
+                      <Link to="/createServices" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                        Create Service
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/editProfile" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                        Edit Profile
+                      </Link>
+                    </li>
+                    <li className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                    <span className="material-symbols-outlined">logout</span>
+                    <Link to='/'>logout</Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
-            <Link to="/contact" className={linkStyles.base}>
-              Contact
-            </Link>
-            <Link to="/" className={linkStyles.base}>
-            <span className="material-symbols-outlined">
-            logout
-            </span>
-            </Link>
-           
-          </div>
+          </ul>
         </div>
       </div>
     </nav>
@@ -58,3 +59,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
