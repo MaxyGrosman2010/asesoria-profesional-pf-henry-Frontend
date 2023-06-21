d8370366d45ba0a85a5e172cafe7a0fae1ce5e15
santi carrizo <santiagocarrizo53@gmail.com>
2023-06-20 21:14:41 -0300

    allservices

--- client/asesProf/src/views/AllServices.jsx
+++ client/asesProf/src/views/AllServices.jsx
@@ -5,9 +5,9 @@ import Card from "./Card";
 
 const AllServices = () => {
   return (
-    <div class="flex flex-col justify-center items-center">
-      <div class="flex justify-center mt-10 w-1/2 items-center h-1/20">
-        <input type="text" placeholder="type a service..." class="p-2 rounded-10 border-none outline-none" />
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex justify-center mt-10 w-1/2 items-center h-1/20">
+        <input type="text" placeholder="type a service..." classname="p-2 rounded-10 border-none outline-none" />
         <button
           type="submit"
           class="py-1.5 px-4"
@@ -28,7 +28,26 @@ const AllServices = () => {
         </select>
         <button>show all services</button>
       </div>
-      <div className="card"><Card/></div>
+      <div  className="col-span-7 row-span-4 flex justify-around items-center ">
+        <div className="div3 bg-white flex flex-col items-center justify-center m-5"  style={{ height: "500px", width: "350px" }}>
+          <img src="" alt="" />
+          <h2>SERVICE</h2>
+          <p>Lorem ipsum Lorem ipsum</p>
+          <button>More info</button>
+        </div>
+        <div className="div3 bg-white flex flex-col items-center justify-center m-5" style={{ height: "500px", width: "350px" }}>
+          <img src="" alt="" />
+          <h2>SERVICE</h2>
+          <p>Lorem ipsum Lorem ipsum</p>
+          <button>More info</button>
+        </div>
+        <div className="div3 bg-white flex flex-col items-center justify-center m-5" style={{ height: "500px", width: "350px" }}>
+          <img src="" alt="" />
+          <h2>SERVICE</h2>
+          <p>Lorem ipsum Lorem ipsum</p>
+          <button>More info</button>
+        </div>
+      </div>
       <div className="paginado">
        <ul>
         <li>
