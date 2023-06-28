f94238f2a6ba094f64d685bb4a971474ac683ce7
goku673 <zapatamorato@gmail.com>
2023-06-28 01:31:06 -0400

    ruta name

--- client/asesProf/src/components/searchBar/SearchBar.jsx
+++ client/asesProf/src/components/searchBar/SearchBar.jsx
@@ -1,15 +1,26 @@
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import Swal from "sweetalert2"
+
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useEffect,useState } from "react";
+import {useDispatch} from 'react-redux';
+import  {getServiceName} from '../../Redux/actions';
+
 
 const SearchBar = ({copyState, updateFilter}) => {
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const [searchValue, setSearchValue] = useState('')
 
   const eventChange = (event) => {
     setSearchValue(event.target.value)
   }
+
+
+  useEffect(() => {
+     dispatch(getServiceName(searchValue));
+  },[dispatch,searchValue]);
   
   const handleSearch = (event) => {
     event.preventDefault()
@@ -33,9 +44,9 @@ const SearchBar = ({copyState, updateFilter}) => {
   }
 
   return (
-    <div className="mt-10">
+    <div className="my-10">
       <form onSubmit={handleSearch} className="flex gap-2 items-center justify-center h-[40px]">
-        <input value={searchValue} onChange={eventChange} className="w-[600px] bg-transparent bg-slate-200 py-1 px-2 placeholder-slate-500" placeholder="search a service..." />
+        <input value={searchValue} onChange={eventChange} className="w-[600px] bg-slate-100 py-1 px-2 placeholder-slate-500" placeholder="search a service..." />
         <button type="submit" className="bg-slate-900 text-white w-[60px] rounded">
           <div className="h-[30px] flex items-center justify-center">
             <span className="material-symbols-outlined">search</span>
@@ -47,4 +58,4 @@ const SearchBar = ({copyState, updateFilter}) => {
   )
 }
 
-export default SearchBar
+export default SearchBar;
