4003a8400070f063c20cd96cfb822b164609ff35
FedeOrefici <federicoaorefici@gmail.com>
2023-06-30 09:27:41 -0300

    update

--- client/asesProf/src/views/filters/Filters.jsx
+++ client/asesProf/src/views/filters/Filters.jsx
@@ -1,86 +1,93 @@
-import { useState,useEffect} from "react";
-import { useSelector } from "react-redux";
+import { useState,useEffect} from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { filter, getData, getTypeServices } from "../../Redux/actions"
 
 
 
-const Filters = ({copyState, updateFilterSelect}) => {
-  
-  const [filterBy,setFilterBy] = useState('');
-  const [filterByProf,setFilterByProf] = useState('');
-  const typeServices = useSelector((state) => state.typeServices);
+const Filters = ({setPage}) => {
+ 
+  const dispatch = useDispatch()
+  const copyState = useSelector((state) => state.copyState)
+
 
   useEffect(() => {
-    filterSelect();
-    filterSelectProf();
-  },[filterBy,filterByProf]);
-   
-  const filterSelect = () => {
-      let filtros = [...copyState];
-      if(filterBy === 'a-z'){
-        filtros = filtros.sort((a,b) => a.name.localeCompare(b.name));
-      }else if ( filterBy === 'z-a'){
-        filtros = filtros.sort((a,b) => b.name.localeCompare(a.name));
-      }else if (filterBy ==='ma-mi'){
-        filtros = filtros.sort((a,b) => a.price - b.price);
-      }else if (filterBy ==='mi-ma' ){
-        filtros = filtros.sort((a,b) => b.price - a.price);
-      }
-      updateFilterSelect(filtros);
-  } 
+    dispatch(getTypeServices())
+  }, [])
+  
 
-  const filterSelectProf = () => {
-    let filtros = [...copyState];
- 
-    switch (filterByProf) {
-      case 'g':
-        filtros  = filtros.filter((prof) => prof.tipoServ === "Gastronomia'"); break;
-      case 'l':
-        filtros  = filtros.filter((prof) => prof.tipoServ === "Legal");break;
-      case 'm':
-        filtros  = filtros.filter((prof) => prof.tipoServ === "Medicina");break;
-      case 'a':
-        filtros  = filtros.filter((prof) => prof.tipoServ === "Automotor"); break;
-      case 'c':
-        filtros  = filtros.filter((prof) => prof.tipoServ === "Construccion");break;
-      case 'e':
-        filtros  = filtros.filter((prof) => prof.tipoServ === "Educacion");break;
-      case 't':
-        filtros  = filtros.filter((prof) => prof.tipoServ === "Tech");break;
-      case 'o':
-        filtros  = filtros.filter((prof) => prof.tipoServ === "Otros"); break;
-      default:
-        filtros = filtros;
-        break;
-    }
- 
-    updateFilterSelect(filtros);
+  const [order, setOrder] = useState('')
+  const [orderService, setOrderService] = useState('')
+  const [filterByType, setFilterByType] = useState('')
+  const [originalServices, setOriginalServices] = useState([]);
+  const [filteredServices, setFilteredServices] = useState([]);
+
+  useEffect(() => {
+    setOriginalServices(copyState);
+  }, [copyState]);
+
+  const handleOrder = () => {
+    let services = [...copyState]
+    if(order === 'asc') services = services.sort((a, b) => a.name.localeCompare(b.name))
+    if(order === 'des') services = services.sort((a, b) => b.name.localeCompare(a.name))
+    if(order === 'high') services = services.sort((a, b) => b.price - a.price)
+    if(order === 'low') services = services.sort((a, b) => a.price - b.price)
+    dispatch(filter(services))
   }
 
+  const selectTypeService = (event) => {
+    const selectedType = event.target.value;
+    let services = [...originalServices];
+    setFilterByType(selectedType);
+  
+    if (selectedType === "all") {
+      dispatch(getData())
+    } else {
+      const filteredServices = services.filter(
+        (srv) => srv.typeService === selectedType
+      );
+      setFilteredServices(filteredServices);
+      dispatch(filter(filteredServices));
+    }
+    setPage(1)
+  };
+
+  useEffect(() => {
+    handleOrder()
+    setPage(1)
+  }, [order])
+  
 
   return (
     <div className="mt-10">
-        <select className="bg-salte-300 rounded w-[200px] text-center py-2 mx-2" onChange={(event) => setFilterBy(event.target.value)}>
+
+        <select value={order} onChange={(event) => setOrder(event.target.value)} className="bg-salte-300 rounded w-[200px] text-center py-2 mx-2" >
           <option value=''>select by</option>
-          <option value='a-z'>A-Z</option> 
-          <option value='z-a'>Z-A</option>
-          <option value='ma-mi'>major minor price</option>
-          <option value='mi-ma'>major minor price</option>
+          <option value='asc'>A-Z</option>
+          <option value='des'>Z-A</option>
+          <option value='high'>High price</option>
+          <option value='low'>Low price</option>
         </select>
-        <select className="bg-salte-300 rounded w-[200px] text-center py-2 mx-2" onChange={(event) => setFilterByProf(event.target.value)} >
-        <option>Select a Service type</option>
-            <option value="g">Gastronomia</option>
-            <option value="l">Legales</option>
-            <option value="m">Medicina</option>
-            <option value="a">Automotor</option>
-            <option value="c">Construcción</option>
-            <option value="e">Enseñanza</option>
-            <option value="t">Tech</option>
-            <option value="o">Otros</option>
-        </select>
+
+        <select
+        value={filterByType}
+        className="bg-salte-300 rounded w-[200px] text-center py-2 mx-2"
+        onChange={selectTypeService}
+      >
+        <option value="all">Select a Service type</option>
+        <option value="Gastronomia">Gastronomia</option>
+        <option value="Legal">Legal</option>
+        <option value="Medicina">Medicina</option>
+        <option value="Automotor">Automotor</option>
+        <option value="Construcción">Construcción</option>
+        <option value="Educación">Educación</option>
+        <option value="Tech">Tech</option>
+        <option value="Otros">Otros</option>
+      </select>
+
 
     </div>
   )
 }
 
-export default Filters;
 
+export default Filters;
